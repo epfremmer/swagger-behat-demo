d0dddcf17dcdb64672d2fbfa29abc0b246ea3e5c
epfremme <epfremme@nerdery.com>
2015-06-29 01:15:15 -0500

    Added initial app resources

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony-behat-demo
+==================
+
+A Symfony project created on June 28, 2015, 2:57 pm.
---
+++ web/app_dev.php
@@ -0,0 +1,28 @@
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+//umask(0000);
+defined('SYMFONY_ENV') || define('SYMFONY_ENV', (getenv('SYMFONY_ENV') ? getenv('SYMFONY_ENV') : 'prod'));
+if (
+    SYMFONY_ENV !== 'dev'
+    && SYMFONY_ENV !== 'staging'
+    && (
+        isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+    )
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
+require_once __DIR__.'/../app/AppKernel.php';
+$kernel = new AppKernel(SYMFONY_ENV, true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
